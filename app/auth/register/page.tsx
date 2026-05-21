b99777a7842dfd12a3f5a93602619fe162e4f8da
Brenton Forwood <forwoodb77@gmail.com>
2026-05-21 03:10:17 -0400

    Created register form

--- app/auth/register/page.tsx
+++ app/auth/register/page.tsx
@@ -1,8 +1,32 @@
+import Link from "next/link";
+
 const RegisterPage = () => {
   return (
-    <>
-      <h1>Register Page</h1>
-    </>
+    <div className="flex flex-col items-center">
+      <h2>Register</h2>
+      <form action="">
+        <fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4">
+          <legend className="fieldset-legend">Login</legend>
+
+          <label className="label">Email</label>
+          <input type="email" className="input" placeholder="Email" />
+
+          <label className="label">Password</label>
+          <input type="password" className="input" placeholder="Password" />
+
+          <button className="btn btn-neutral mt-4">Login</button>
+        </fieldset>
+      </form>
+      <p>
+        Already have an account?{" "}
+        <Link
+          href={`/auth/login`}
+          className="h-4 border-b border-blue-800 text-blue-800"
+        >
+          Log In
+        </Link>
+      </p>
+    </div>
   );
 };
 
