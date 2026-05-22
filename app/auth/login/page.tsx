79f62132f233ee0c50c77699f9fae2754233a2b6
Brenton Forwood <forwoodb77@gmail.com>
2026-05-22 17:11:02 -0400

    Added AuthForm component to Login page

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -1,9 +1,11 @@
+import AuthForm from "@/app/components/AuthForm";
 import React from "react";
 
 const LoginPage = () => {
   return (
     <>
       <h1>Login Page</h1>
+      <AuthForm />
     </>
   );
 };
