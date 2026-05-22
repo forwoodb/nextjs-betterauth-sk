6e18bce67d552dd5c0574169686f488fa250bc69
Brenton Forwood <forwoodb77@gmail.com>
2026-05-22 17:47:46 -0400

    Added route protection to protected-route page

--- app/protected-route/page.tsx
+++ app/protected-route/page.tsx
@@ -1,7 +1,22 @@
-const ProtectedPage = () => {
+import { headers } from "next/headers";
+import { auth } from "../lib/auth";
+import { redirect } from "next/navigation";
+
+const ProtectedPage = async () => {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+
+  if (!session) {
+    redirect("/auth/login");
+  }
+
+  console.log(session);
+
   return (
     <>
       <h1>Protected Dashboard Page</h1>
+      <p>Hello {session?.user.name}</p>
     </>
   );
 };
