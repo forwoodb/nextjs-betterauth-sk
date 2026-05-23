ae33dfa7bc15893c32cc454c0ceb068b77c9f4c5
Brenton Forwood <forwoodb77@gmail.com>
2026-05-23 05:52:21 -0400

    Retrieved user data from db

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,11 +1,30 @@
 import { connectDb } from "../lib/mongodb";
+import { User } from "../models/User";
 
 const AdminPage = async () => {
   await connectDb();
 
+  const users = User.find({});
+
   return (
     <main>
       <h1>Admin Page</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+          </tr>
+        </thead>
+        <tbody>
+          {(await users).map((user) => {
+            return (
+              <tr key={user._id}>
+                <td>{user.name}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
     </main>
   );
 };
